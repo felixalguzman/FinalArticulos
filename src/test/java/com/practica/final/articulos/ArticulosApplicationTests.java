9b7f9053fce148e0596d700486b5a6ef7abbfd62
Felix A. Guzman <felixalejandro@outlook.com>
2018-12-13 11:55:32 -0400

    Creando proyecto de artivulos

---
+++ src/test/java/com/practica/final/articulos/ArticulosApplicationTests.java
@@ -0,0 +1,17 @@
+package com.practica.final.articulos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArticulosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
